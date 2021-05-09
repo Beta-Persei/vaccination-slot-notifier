f7227f87c41c314d9fccc33d93f0fb1e4c7d5fc0
Daniyal <abbasi.daniyal98@gmail.com>
2021-05-09 11:15:53 +0530

    [ADD] whatsapp integration and phone number field

--- subscribers/tests.py
+++ subscribers/tests.py
@@ -1,3 +1,191 @@
+from http import HTTPStatus
 from django.test import TestCase
+from django.contrib.messages import get_messages
 
-# Create your tests here.
+from subscribers.forms import SubscriberForm
+
+
+class SubscriberFormTests(TestCase):
+    def test_valid_form_data(self):
+        valid_form_data = [
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 45,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 111111,
+                "state": "",
+                "district_id": "",
+                "search_type": "pincode",
+            },
+        ]
+        for data in valid_form_data:
+            with self.subTest(data):
+                form = SubscriberForm(data=data)
+                self.assertTrue(form.is_valid())
+
+    def test_invalid_form_data(self):
+        invalid_form_data = [
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+91999999999",
+                "age_limit": 45,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 1,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 1,
+                "state": "",
+                "district_id": "",
+                "search_type": "pincode",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": "",
+                "state": 1,
+                "district_id": 2,
+                "search_type": "pincode",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 1,
+                "state": "",
+                "district_id": "",
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": "",
+                "state": 1,
+                "district_id": "TEST",
+                "search_type": "district",
+            },
+        ]
+        for data in invalid_form_data:
+            with self.subTest(data):
+                form = SubscriberForm(data=data)
+                self.assertFalse(form.is_valid())
+
+
+class SubscriberTests(TestCase):
+    def test_valid_form_data(self):
+        valid_form_data = [
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 45,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 111111,
+                "state": "",
+                "district_id": "",
+                "search_type": "pincode",
+            },
+        ]
+        for data in valid_form_data:
+            with self.subTest(data):
+                response = self.client.post("/", data=data)
+                self.assertRedirects(response, "/")
+                messages = list(get_messages(response.wsgi_request))
+                self.assertEqual(len(messages), 1)
+                self.assertEqual(
+                    str(messages[0]),
+                    "You will be notified via email whenever a vaccinaton slot is available!",
+                )
+
+    def test_invalid_form_data(self):
+        invalid_form_data = [
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+91999999999",
+                "age_limit": 45,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 1,
+                "pincode": "",
+                "state": 1,
+                "district_id": 1,
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 1,
+                "state": "",
+                "district_id": "",
+                "search_type": "pincode",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": "",
+                "state": 1,
+                "district_id": 2,
+                "search_type": "pincode",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": 1,
+                "state": "",
+                "district_id": "",
+                "search_type": "district",
+            },
+            {
+                "email": "foo@bar.com",
+                "phone_number": "+919999999999",
+                "age_limit": 18,
+                "pincode": "",
+                "state": 1,
+                "district_id": "TEST",
+                "search_type": "district",
+            },
+        ]
+        for data in invalid_form_data:
+            with self.subTest(data):
+                response = self.client.post("/", data=data)
+                self.assertEqual(response.status_code, 200)
--- subscribers/utils.py
+++ subscribers/utils.py
@@ -1,13 +1,19 @@
 import requests
 from datetime import datetime
+import textwrap
 
 from django.core.mail import send_mail
 from django.shortcuts import resolve_url
 from django.template.loader import render_to_string
 from django.conf import settings
+from twilio.rest import Client
 
-from vaccination_slot_notifier import celery_app
 from sniffer.api import get_centers_by_district_id, get_centers_by_pincode
+from sniffer.models import Slots
+from vaccination_slot_notifier import celery_app
+
+
+twilio_client = Client(settings.TWILIO_ACCOUND_SID, settings.TWILIO_AUTH_TOKEN)
 
 
 @celery_app.task
@@ -18,41 +24,80 @@ def send_slot_mail(subscriber_email, message):
     )
 
 
-def parse_centers(centers, age_limit):
+@celery_app.task
+def send_slot_whatsapp(subscriber_number, message):
+    message = twilio_client.messages.create(
+        body=message,
+        from_=f"whatsapp:{settings.TWILIO_PHONE_NUMBER}",
+        to=f"whatsapp:{subscriber_number}",
+    )
+
+
+def create_mail_string(slots):
     message_text = ""
+    for slot in slots:
+        slot_text = ", ".join(slot.slots)
+        date_text = slot.date.strftime("%d-%m-%Y")
+        message_text += f"""
+Center : {slot.name}, {slot.address}, {slot.district_name}, {slot.state_name}, {slot.pincode}
+Date : {date_text}
+Vaccine : {slot.vaccine}
+Slot Timings : {slot_text}
+Total Slots available : {slot.available_capacity}
+
+    """
+    return message_text
+
+
+def create_message_strings(slots):
+    message_texts = []
+    message_text = "Vaccination slot found!\n"
+    for slot in slots:
+
+        slot_text = ", ".join(slot.slots)
+        date_text = slot.date.strftime("%d-%m-%Y")
+        center_text = f"""
+{slot.name}, {slot.address}, {slot.district_name}, {slot.state_name}, {slot.pincode} on {date_text}. Slots available {slot.available_capacity} ({slot.vaccine})
+
+        """
+        if len(center_text) + len(message_text) >= settings.TWILIO_MAX_SMS_SIZE:
+            message_texts.append(message_text)
+            message_text = center_text
+        else:
+            message_text += center_text
+    message_texts.append(message_text)
+    return message_texts
+
+
+def filter_centers(centers, age_limit):
+    filtered_slots = []
     for center in centers:
         for session in center.sessions:
             if session.min_age_limit == age_limit and session.available_capacity > 0:
-                slot_text = ", ".join(session.slots)
-                date_text = session.date.strftime("%d-%m-%Y")
-                message_text += f"""
-Center name : {center.name}
-Center address : {center.address}, {center.district_name}, {center.state_name}, {center.pincode}
-Date : {date_text}
-Vaccine : {session.vaccine}
-Slot Timings : {slot_text}
-Total Slots available : {session.available_capacity}
-
-                """
-    return message_text
+                filtered_slots.append(Slots.from_center_session(center, session))
+    return filtered_slots
 
 
 def check_and_notify_subscriber(subscriber):
     if subscriber.pincode:
         centers = get_centers_by_pincode(subscriber.pincode)
-    else:
+    elif subscriber.pincode:
         centers = get_centers_by_district_id(subscriber.district_id)
+    else:
+        return
 
-    if centers:
-        centers_string = parse_centers(centers, subscriber.age_limit)
-        if centers_string:
+    filtered_slots = filter_centers(centers, subscriber.age_limit)
+    if filtered_slots:
+        if subscriber.email:
             message = render_to_string(
                 "subscribers/notification_mail.html",
                 {
                     "domain": settings.SITE_HOST_DOMAIN,
-                    "centers": centers_string,
+                    "centers": create_mail_string(filtered_slots),
                     "subscriber_id": subscriber.id,
                 },
             )
-
             send_slot_mail.delay(subscriber.email, message)
+        if subscriber.phone_number:
+            for message in create_message_strings(filtered_slots):
+                send_slot_whatsapp.delay(subscriber.phone_number.as_e164, message)
