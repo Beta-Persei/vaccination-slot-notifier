3883281e4263ffc5b320d4c89a34cbee9edec795
Daniyal <abbasi.daniyal98@gmail.com>
2021-05-11 11:45:55 +0530

    [ADD] Constance settings

--- subscribers/tasks.py
+++ subscribers/tasks.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from django.conf import settings
+from constance import config
 
 from subscribers.models import Subscriber
 from vaccination_slot_notifier import celery_app
@@ -12,7 +12,7 @@ def check_available_slots():
     subscriber_list = Subscriber.objects.filter(
         active=True,
         last_mail_sent__lt=datetime.now()
-        - timedelta(seconds=settings.MAIL_COOLDOWN_SECONDS),
+        - timedelta(seconds=config.MAIL_COOLDOWN_SECONDS),
     )
     for subscriber in subscriber_list:
         check_and_notify_subscriber(subscriber)
@@ -21,5 +21,5 @@ def check_available_slots():
 @celery_app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
-        settings.SNIFFING_INTERVAL_SECONDS, check_available_slots.s()
+        config.SNIFFING_INTERVAL_SECONDS, check_available_slots.s()
     )
