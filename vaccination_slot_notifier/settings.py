3463a369e9e3fc07a651ba4f1cb195c78d4e7ee6
Daniyal Abbasi <abbasi.daniyal98@gmail.com>
2021-05-11 12:28:51 +0530

    Revert "[Fix] Debug settings and updated .env.sample"
    This reverts commit bea32521812327b85310f55087e04d06ff2ea36b.

--- vaccination_slot_notifier/settings.py
+++ vaccination_slot_notifier/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG")))
-
+DEBUG = os.environ.get("DEBUG", default=True)
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
@@ -180,7 +179,7 @@ PHONENUMBER_DEFAULT_REGION = "IN"
 
 # Constance Settings
 CONSTANCE_CONFIG = {
-    "SMS_SERVICE_ACTIVE": (False, "Whether SMS service is active or not"),
-    "SNIFFING_INTERVAL_SECONDS": (300, "Seconds between each sniffer iteration"),
-    "MAIL_COOLDOWN_SECONDS": (1800, "Seconds before a new mail is sent to a user."),
+    'SMS_SERVICE_ACTIVE': (False, "Whether SMS service is active or not"),
+    'SNIFFING_INTERVAL_SECONDS': (300, "Seconds between each sniffer iteration"),
+    'MAIL_COOLDOWN_SECONDS': (1800, "Seconds before a new mail is sent to a user."),
 }
