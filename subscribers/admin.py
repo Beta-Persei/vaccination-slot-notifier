a065d6b06ca825a24ac3a9144244ec57a31a600f
Daniyal <abbasi.daniyal98@gmail.com>
2021-05-13 02:08:52 +0530

    [ADD] Filters to Admin

--- subscribers/admin.py
+++ subscribers/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from subscribers.models import Subscriber
 
-admin.site.register(Subscriber)
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_filter = ('email', 'phone_number', 'pincode', 'district_id', 'age_limit', 'active')
+    actions_selection_counter = True
